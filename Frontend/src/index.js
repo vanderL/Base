a42cec2ce0e0ab3ca2da70d6c95560e36340f777
vanderL <vnd.vander@gmail.com>
2021-04-21 22:37:49 -0300

    Creating source component

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,3 +1,6 @@
-const soma = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
 
-alert(soma(1, 8));
+import App from './app';
+
+render(<App></App>, document.getElementById('app'));
